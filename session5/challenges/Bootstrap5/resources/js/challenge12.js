cd8305a41b099b51f09ab3129d735c6be5e4dce0
Ryan2409 <ryan.ng.2022@scis.smu.edu.sg>
2023-09-15 14:50:24 +0800

    did abit of challenge12

--- session5/challenges/Bootstrap5/resources/js/challenge12.js
+++ session5/challenges/Bootstrap5/resources/js/challenge12.js
@@ -9,6 +9,32 @@
 function highlight_words() {
 
     // YOUR CODE GOES HERE
+    if(false){
+        
+    }else{
+        var len = prompt("How long word, more than this will be highlighted");
+        // let body = document.getElementById("book_chapter");
+        // let all_p = body.getElementsByTagName("p");
+        var paras = document.querySelectorAll('#book_chapter p');
+
+        for(para of paras){
+            let arr = ["<p>"];
+            let split = para.innerText.split(" ");
+            for (word of split){
+                if(word.length > len){
+                    arr.push(`<span style="background-color:yellow;">${word}</span>`);
+                }else{
+                    arr.push(word);
+                }
+            }
+            arr.push("</p>");
+            arr = arr.join(" ");
+            // console.log(arr);
+            para.innerHTML = arr;
+            console.log(para);
+        }
+
+    }
 }
 
 
